a6ce595e4161307dc8dc9556e1e0e5f77f317967
thomas011001 <thomasyacoub321@gmail.com>
2025-09-13 01:46:09 +0300

    feat: add rate limiting to login route to prevent brute force attacks

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -4,10 +4,19 @@ import {
   loginValidator,
   signupValidator,
 } from "../validators/authValidator.js";
+import rateLimit from "express-rate-limit";
 const authRouter = Router();
 
+const loginLimiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 5,
+  message: "Too many login attempts, please try again after a minute.",
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
 authRouter.post("/signup", signupValidator, signup);
-authRouter.post("/login", loginValidator, login);
+authRouter.post("/login", loginValidator, loginLimiter, login);
 authRouter.post("/logout", logout);
 
 export default authRouter;
