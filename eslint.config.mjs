eb855c7e4325d6c2c416afce04a533648d46f63f
thomas011001 <thomasyacoub321@gmail.com>
2025-09-12 22:49:07 +0300

    chore: update devDependencies to include ESLint and related packages

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+import js from "@eslint/js";
+import globals from "globals";
+import markdown from "@eslint/markdown";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
+]);
