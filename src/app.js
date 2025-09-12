b23440a09f032273fcfc76fe6eeb0a5e6bb31286
thomas011001 <thomasyacoub321@gmail.com>
2025-09-13 01:45:56 +0300

    feat: add apicache and morgan for caching and logging middleware

--- src/app.js
+++ src/app.js
@@ -5,12 +5,17 @@ import authRouter from "./routes/auth.route.js";
 import globalErrorHandler from "./middleware/globalErrorHandler.js";
 import userRouter from "./routes/user.route.js";
 import postRouter from "./routes/post.route.js";
+import morgan from "morgan";
+import apicache from "apicache";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use(morgan("dev"));
+app.use(apicache.middleware("5 minutes"));
 
 app.use("/", authRouter);
 app.use("/users", userRouter);
