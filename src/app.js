f25e27f1510f0c24d7812202d0a33d6ca818ff40
thomas011001 <thomasyacoub321@gmail.com>
2025-09-20 03:07:01 +0300

    fix: update CORS origin to allow requests from the main application URL

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173/login",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
